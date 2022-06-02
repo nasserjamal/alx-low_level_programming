49e99218b10dc1010621b9701a76c219ea756a9e
Nasser jamal <nasserjamal30@gmail.com>
2022-06-02 07:35:37 -0700

    last assignment

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-	const char msg[] = 
-"and that piece of art is useful\" - Dora Korpar,2015-10-19\n";
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar,\
+2015-10-19\n";
 
 	write(1, msg, sizeof(msg));
 	return (1);
