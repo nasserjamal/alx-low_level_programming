ef35a68bbe49d7bde875a75136f0880083968c80
Nasser jamal <nasserjamal30@gmail.com>
2022-06-02 04:12:47 -0700

    Great

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,9 @@
  */
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1,msg,sizeof(msg));
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar,"
+		" 2015-10-19\n";
+	
+	write(1, msg, sizeof(msg));
 	return (1);
 }
