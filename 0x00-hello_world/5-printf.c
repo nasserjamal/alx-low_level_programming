860c31897db7a53cac5f8325bf73b80cecc1b45f
Nasser jamal <nasserjamal30@gmail.com>
2022-06-02 02:24:38 -0700

    this is wrong!

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This function is the entry point
+ *
+ * Return: Returns a 0 always
+ */
+int main (void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
