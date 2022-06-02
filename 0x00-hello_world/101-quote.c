6bf745053a1a2c0c77e436f6157403c5a7f1caa4
Nasser jamal <nasserjamal30@gmail.com>
2022-06-02 04:37:04 -0700

    jhkjd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-#include <sys/syscall.h>
 #include <unistd.h>
 
 /**
  * main - Prints out the result
  *
- * Return: returns 1. As the output
+ * Return: returns 1 As the output
  */
 int main(void)
 {
 	const char msg[] = "and that piece of art is useful\" - Dora Korpar,"
 		" 2015-10-19\n";
 	
-	write(1, msg, sizeof(msg));
+	write(1,msg,sizeof(msg));
 	return (1);
 }
