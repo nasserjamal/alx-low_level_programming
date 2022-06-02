ee7a41368b2f1bda6970e67d0bfa497f2465a775
Nasser jamal <nasserjamal30@gmail.com>
2022-06-02 02:01:27 -0700

    this is wrong!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
-int main(void){
+/*Bettyy utaniuwwaa. mwenyewe sina time hapa*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
