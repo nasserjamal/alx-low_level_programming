5c727d8e64332c210d2b43a486c22a37d27e3404
Nasser jamal <nasserjamal30@gmail.com>
2022-06-02 02:27:11 -0700

    this is wrong!

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
  *
  * Return: Returns a 0 always
  */
-int main (void)
+int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
