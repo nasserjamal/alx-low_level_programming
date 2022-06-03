a4bce3ff776ee2c54a56c698d342117a72288c2b
Nasser jamal <nasserjamal30@gmail.com>
2022-06-03 05:38:53 -0700

    advanced

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	int i = j = 0;
+	int i = 0;
+	int j = 0;
 
 	for (i = 0; i <= 9; i++)
 	{
@@ -20,11 +21,11 @@ int main(void)
 				if (i != 8 && j != 9)
 				{
 					putchar(',');
-					putchar(" ");
+					putchar(' ');
 				}
 			}
 		}
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
