b4186f7889ee2599b7eb684ec4f3669c698ac61f
Nasser Jamal <nasserjamal30@gmail.com>
2022-06-07 18:59:41 +0000

    another

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point to the program
+ *
+ * Return: true
+ */
+int main(void)
+{
+	int i=0;
+	int z = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+		i++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
