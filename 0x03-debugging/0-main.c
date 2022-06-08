d74243fd782a5d65359b7d918e6dbdced5b83620
Nasser Jamal <nasserjamal30@gmail.com>
2022-06-08 12:02:42 +0000

    Debugging task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Is the entry point to the file
+ *
+ * Return: Returns success
+ */
+int main(void)
+{
+	int i = 0;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
