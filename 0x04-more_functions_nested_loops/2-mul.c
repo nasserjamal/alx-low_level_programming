2520251b5db5889e0a4e3af84cea010155ab43db
Nasser Jamal <nasserjamal30@gmail.com>
2022-06-10 08:30:39 +0000

    Assignments on nestsed loops

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,9 +5,7 @@
  * mul - multiplies two arguments
  * @x: argument 1
  * @y: argument 2
- * @result: result of the multiplication
  * Return: Returns result
- * Return: Always 0 Success
  */
 
 int mul(int x, int y)
