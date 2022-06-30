13c417d0f9cc092d89a0195f38c602c521f1316e
Nasser Jamal <nasserjamal30@gmail.com>
2022-06-30 19:19:43 +0000

    pointers and malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * array_range - creates an array of integers.
@@ -8,4 +9,19 @@
  */
 int *array_range(int min, int max)
 {
+	int *array;
+	int size;
+	int i;
+
+	size = (max - min) + 1;
+
+	if (min > max)
+		return (NULL);
+
+	array = malloc(sizeof(int) * size);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		array[i] = min++;
+	return (array);
 }
