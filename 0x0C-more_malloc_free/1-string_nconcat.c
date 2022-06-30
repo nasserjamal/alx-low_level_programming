929b679df98c0a75d674d2f2a9336ca1d15405c8
Nasser Jamal <nasserjamal30@gmail.com>
2022-06-30 18:35:25 +0000

    pointers and malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,6 +16,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (newString == NULL)
 		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (*s1)
 	{
 		newString[i] = *s1;
