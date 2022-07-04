21d1c192a717c9261248710a5f7d00b71fa8fe00
Nasser Jamal <nasserjamal30@gmail.com>
2022-07-04 14:17:53 +0000

    free dog struct

--- 0x0E-structures_typedef/4-main.c
+++ 0x0E-structures_typedef/4-main.c
@@ -11,6 +11,7 @@ int main(void)
     dog_t *my_dog;
 
     my_dog = new_dog("Poppy", 3.5, "Bob");
+    if(my_dog==NULL) return(0);
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
     return (0);
 }
