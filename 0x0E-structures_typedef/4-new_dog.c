50b221083d14b297156e6afc4bcccf0277bde2e8
Nasser Jamal <nasserjamal30@gmail.com>
2022-07-04 13:38:35 +0000

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,19 +11,19 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-char *storedName = malloc(sizeof(char) * (strlen(name)+1));
-char *storedOwner = malloc(sizeof(char) * (strlen(owner)+1));
-dog_t *newDog;
+	char *storedName = malloc(sizeof(char) * (strlen(name) + 1));
+	char *storedOwner = malloc(sizeof(char) * (strlen(owner) + 1));
+	dog_t *newDog;
 
-newDog = malloc(sizeof(dog_t));
-if(newDog == NULL)
-return (NULL);
-if (name == NULL || owner == NULL || age < 0)
-return (NULL);
-strcpy(storedName, name);
-strcpy(storedOwner, owner);
-newDog->name = storedName;
-newDog->age = age;
-newDog->owner = storedOwner;
-return (newDog);
+	newDog = malloc(sizeof(dog_t));
+	if (newDog == NULL)
+		return (NULL);
+	if (name == NULL || owner == NULL || age < 0)
+		return (NULL);
+	strcpy(storedName, name);
+	strcpy(storedOwner, owner);
+	newDog->name = storedName;
+	newDog->age = age;
+	newDog->owner = storedOwner;
+	return (newDog);
 }
