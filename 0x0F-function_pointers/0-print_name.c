4c58156964559257bfec47c59f6cc145505d7230
Nasser Jamal <nasserjamal30@gmail.com>
2022-07-06 13:38:14 +0000

    checker

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,11 @@
  * print_name - Prints a name
  * @name: name to be printed
  * @f: Function pointer that prints the name
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
 if (name == NULL || f == NULL)
-return;
+	return;
 f(name);
 }
