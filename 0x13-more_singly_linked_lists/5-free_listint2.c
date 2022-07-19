bdf84767dbe591f5d23296acb43320b0232a7c2e
Nasser Jamal <nasserjamal30@gmail.com>
2022-07-19 15:34:04 +0300

    Freelist2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,10 +8,22 @@
 void free_listint2(listint_t **head)
 {
 if (head != NULL)
-if (*head != NULL)
 {
-if ((*head)->next != NULL)
-free_listint2(&((*head)->next));
-free(*head);
+free_listint(*head);
+(*head) = NULL;
+}
+}
+
+/**
+ * free_listint - frees all nodes
+ *
+ * @head: address of head
+ */
+void free_listint(listint_t *head)
+{
+if (head != NULL)
+{
+free_listint(head->next);
+free(head);
 }
 }
