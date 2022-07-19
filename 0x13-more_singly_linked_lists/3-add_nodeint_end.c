9d06dba5aac06f8ec57654ca9093279b6901828a
Nasser Jamal <nasserjamal30@gmail.com>
2022-07-19 09:54:51 +0000

    Add last node

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,6 +1,45 @@
 #include "lists.h"
 
 /**
+ * addLastNode - adds the final node
+ *
+ * @head: Head node
+ * @last: Currently last node
+ * @n: Value of new last node
+ * Return: (listint_t*)
+ */
+listint_t *addLastNode(listint_t **head, listint_t *last, const int n)
+{
+listint_t *new;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = NULL;
+if (*head == NULL)
+	*head = new;
+else
+(last)->next = new;
+return (new);
+}
+
+/**
+ * iterate - Navigates to the last node
+ * @head: Head node
+ * @current: Current node
+ * @n: Value of last node
+ * Return: (listint_t*) addres of last node
+ */
+listint_t *iterate(listint_t **head, listint_t *current, const int n)
+{
+if (*head != NULL)
+if (current->next != NULL)
+return (iterate((head), (current)->next, n));
+return (addLastNode(head, current, n));
+}
+
+
+/**
  * add_nodeint_end -adds nodes at the end
  * @head -head of the list
  * @n: value added
@@ -8,14 +47,5 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-listint_t *new;
-if ((*head)->next != NULL)
-return (add_nodeint_end(&((*head)->next), n));
-new = malloc(sizeof(listint_t));
-if (new == NULL)
-return (NULL);
-new->n = n;
-new->next = NULL;
-(*head)->next = new;
-return (new);
+return (iterate(head, *head, n));
 }
