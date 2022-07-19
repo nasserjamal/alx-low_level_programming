a58874c525ae3df79167c8b184bc963d01cfefe7
Nasser Jamal <nasserjamal30@gmail.com>
2022-07-19 11:42:47 +0000

    free nodes(debug)

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,9 +4,11 @@
  *
  * @head: address of head
  */
-void free_listint(listint_t *head){
-    if(head != NULL){
-    free_listint(head->next);
-    free(head);
-    }
+void free_listint(listint_t *head)
+{
+if (head != NULL)
+{
+free_listint(head->next);
+free(head);
+}
 }
