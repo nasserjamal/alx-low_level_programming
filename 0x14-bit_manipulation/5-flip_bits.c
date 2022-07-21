4ee74e1ff163c848aae0a9de0bbe7ea4e65a5511
Nasser Jamal <nasserjamal30@gmail.com>
2022-07-21 13:35:42 +0300

    advanced

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,14 +10,14 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-unsigned int c = 0;
+unsigned long int c = 0;
 unsigned int noFlipped = 0;
 
 c = n ^ m;
 while (c > 0)
 {
 if ((c & 1) == 1)
-	noFlipped++;
+noFlipped += (c & 1);
 c = c >> 1;
 }
 return (noFlipped);
