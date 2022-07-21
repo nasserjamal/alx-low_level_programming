757fa40139a57c4ad7913e4c9adbbb9ae0826e5f
Nasser Jamal <nasserjamal30@gmail.com>
2022-07-21 12:46:29 +0300

    advanced

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,6 +8,8 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+if (index >= (sizeof(unsigned long int) * 8))
+return (-1);
 if (n == NULL)
 return (-1);
 *n = (*n & ((*n | ~*n) ^ (1 << index)));
