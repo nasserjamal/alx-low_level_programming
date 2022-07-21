5e4ab8dc32e48b48fe7cc9b84f1fe46992830f82
Nasser Jamal <nasserjamal30@gmail.com>
2022-07-21 12:37:26 +0300

    advanced

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,23 +11,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 if (n == NULL)
 return (-1);
-*n = (*n | power(2, index));
+*n = (*n | 1 >> index);
 return (1);
-}
-
-/**
- * power - Finds power of a number
- * @base: Base power
- * @pow: Power index
- * Return: (int) the value
- */
-long int power(int base, unsigned int pow)
-{
-long int ans = 1;
-while (pow)
-{
-	ans = ans * base;
-	pow--;
-}
-return (ans);
 }
