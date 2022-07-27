149f89c48b314872c80f1d0cdc7c4c0768420b22
Nasser Jamal <nasserjamal30@gmail.com>
2022-07-27 12:44:58 +0300

    readgile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,33 +10,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd, noRead, noWrite;
-char *buf = malloc(sizeof(char) * letters);
-if (buf == NULL)
-return (0);
-if (filename == NULL)
-{
-free(buf);
-return (0);
-}
-fd = open(filename, O_RDONLY);
-if (fd < 0)
-{
-free(buf);
-return (0);
-}
-noRead = read(fd, buf, letters);
-if (noRead < 0)
-{
-free(buf);
-return (0);
-}
-noWrite = write(0, buf, noRead);
-if (noWrite < 0 || noWrite != noRead)
-{
-free(buf);
-return (0);
-}
-free(buf);
-return (noWrite);
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
 }
