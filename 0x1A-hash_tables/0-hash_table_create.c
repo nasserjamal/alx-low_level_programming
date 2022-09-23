4b8f779d2c3486ca4ee7796346126e40c819d918
Nasser Jamal <nasserjamal30@gmail.com>
2022-09-23 09:27:36 +0300

    hash Table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,11 +9,17 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *newTbl = malloc(sizeof(hash_table_t));
 
+	if (newTbl == NULL)
+		return (NULL);
 	newTbl->size = size;
 	newTbl->array = malloc(sizeof(hash_node_t *) * size);
 	if (newTbl->array == NULL)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		newTbl->array[i] = NULL;
+
 	return (newTbl);
 }
